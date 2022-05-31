9f53865e8b98ec659846aa48f0f0d4962e789b5b
Lemony-girl <2529229376@qq.com>
2022-05-31 16:12:27 +0800

    完成了用户模块

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,7 @@ function getUserInfo() {
     //   Authorization: localStorage.getItem('token')
     // },
     success: (res) => {
+      console.log(res)
       if (res.status !== 0) return layer.msg("获取用户信息失败！");
       layer.msg("获取用户信息成功！");
       renderAvatar(res.data)
@@ -17,14 +18,13 @@ function getUserInfo() {
 
 // 渲染用户信息
 const renderAvatar = (user) => {
-  console.log(user);
   let uname = user.nickname || user.username
   // 渲染欢迎语
   $('#welcome').html(`欢迎 ${uname}`)
   //按需渲染头像
-  if (user.uesr_pic !== null) {
+  if (user.user_pic !== null) {
     // 设置图片头像
-    $('.layui-nav-img').attr('src', user.uesr_pic)
+    $('.layui-nav-img').attr('src', user.user_pic)
     $('.text-avatar').hide()
   } else {
     $('.layui-nav-img').hide()
@@ -33,11 +33,14 @@ const renderAvatar = (user) => {
 }
 
 //退出
-$("#btnlogout").click(()=>{
-  layer.confirm('是否退出',{icon:3,title:'提示'},function(index){
+$("#btnlogout").click(() => {
+  layer.confirm('是否退出', {
+    icon: 3,
+    title: '提示'
+  }, function (index) {
     localStorage.removeItem('token')
-    location.href='/login.html'
+    location.href = '/login.html'
   })
-}) 
+})
 
 getUserInfo();
